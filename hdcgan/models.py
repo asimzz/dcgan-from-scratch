bf780b75c9fa8b2b3abf6f56fcd07ec19c3a4c52
Asim Abdalla <asimabdalla99@gmail.com>
2024-06-02 22:07:28 +0000

    add hdcgan implementation

---
+++ hdcgan/models.py
@@ -0,0 +1,80 @@
+import torch.nn as nn
+
+
+class Discriminator(nn.Module):
+    def __init__(self, image_channels):
+        super().__init__()
+
+        self.dicriminator = nn.Sequential(
+            nn.Conv2d(
+                image_channels, 128, kernel_size=4, stride=2, padding=2, bias=False
+            ),
+            nn.SELU(inplace=True),
+            self._block(128, 256, 4, 2, 1),
+            self._block(256, 512, 4, 2, 1),
+            self._block(512, 1024, 4, 2, 1),
+            nn.Conv2d(1024, 1, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.Sigmoid(),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.SELU(inplace=True),
+        )
+
+    def forward(self, x):
+        output = self.dicriminator(x)
+        return output.view(output.shape[0], -1)
+
+
+class Generator(nn.Module):
+    def __init__(self, original_noise_dim, image_channels, zeta=4):
+        super().__init__()
+
+        scaled_noise_dim = int(original_noise_dim * zeta)
+
+        self.fc = nn.Linear(scaled_noise_dim, 1024 * 4 * 4)
+
+        self.generator = nn.Sequential(
+            self._block(1024, 512, 4, 2, 1),
+            self._block(512, 256, 4, 2, 1),
+            self._block(256, 128, 4, 2, 1),
+            nn.ConvTranspose2d(
+                128,
+                image_channels,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=False,
+            ),
+            nn.Tanh(),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size,
+                stride,
+                padding,
+                bias=False,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.SELU(True),
+        )
+
+    def forward(self, x):
+        x = self.fc(x.view(x.size(0), -1))
+        x = x.view(x.size(0), 1024, 4, 4)
+        output = self.generator(x)
+        return output
